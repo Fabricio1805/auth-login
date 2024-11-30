2018caf9fa9842de277bbb9b236e118e8b7b94fc
Fabricio1805 <fabricio.ss09@outlook.com>
2024-11-30 10:31:21 -0300

    fix: mode dto PageableDTO

--- src/main/java/com/auth_login/application/dto/user/ListAllUserResponseDTO.java
+++ src/main/java/com/auth_login/application/dto/user/ListAllUserResponseDTO.java
@@ -2,6 +2,8 @@ package com.auth_login.application.dto.user;
 
 import java.util.List;
 
+import com.auth_login.application.dto.PageableDTO;
+
 public record ListAllUserResponseDTO( List<UserReponseDTO> users,PageableDTO pageable ) {
 
 }
